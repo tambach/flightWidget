bc4a2a0354d6f9274c83bcd551b0349d230be104
Tamuna Bachaliashvili <tamunabachaliashvili@Tamunas-MacBook-Pro.local>
2019-04-03 00:57:26 +0200

    art

--- widget/js/flightWidget.js
+++ widget/js/flightWidget.js
@@ -118,15 +118,15 @@ class FlightController extends WidgetController {
     }
 
     async load() {
-trace("------tickets");
-trace("00--00--00--00");
-        let result = await this.mvc.main.dom("https://www.airfrance.fr/"); // load web page
+trace("------ wikipedia -----");
+
+        let result = await this.mvc.main.dom("https://www.wikipedia.org/"); // load web page
         let domstr = _atob(result.response.dom);                        // decode result
         let parser = new DOMParser();                                   // init dom parser
         let dom = parser.parseFromString(domstr, "text/html");    // inject result
         let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[1]/a').firstResult; // find interesting things
        // let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[1]/a').allResults;
-        trace("res   " + result);
+
         trace(article);
         trace(article.textContent);
         this.mvc.view.update(article.textContent, article.getAttribute("href"));
